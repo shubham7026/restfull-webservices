0bface8b2fa418fed8c456c999ff62d6d31e8cb6
Shubham Vishwakarma <shubhamvishwakarma7026@gmail.com>
2021-01-22 23:31:06 +0530

    Initial commit

---
+++ src/main/java/com/example/restfullwebservice/RestfullWebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.restfullwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfullWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfullWebserviceApplication.class, args);
+	}
+
+}
